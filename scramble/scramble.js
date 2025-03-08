299f373864b17194c2fcad8c126a1b783fc40594
Paulo Galarza <paulo-123@hotmail.es>
2025-03-08 16:45:56 -0500

    Comprobar si la palabra coincide con la desordenada

--- scramble/scramble.js
+++ scramble/scramble.js
@@ -11,13 +11,31 @@ gameArea.appendChild(btn);
 
 // Valores iniciales del juego
 const myWords = ["perro", "gato", "raton"];
-
+const game = {
+  sel: "",
+  scramble: "",
+};
 // evento click del boton
 btn.addEventListener("click", function (e) {
   myWords.sort(() => {
     return 0.5 - Math.random();
   }); // Ordenamos las palabras de forma aleatoria
-  let selWord = myWords[0]; // Seleccionamos la primera palabra
-  output.textContent = `${selWord}`; // Mostramos la palabra en el contenedor
-  console.log(myWords);
+  game.sel = myWords[0]; // Seleccionamos la primera palabra
+  game.scramble = sorter(game.sel); // Llamamos a la funcion sorter
+  output.textContent = `${game.sel} es la palabra seleccionada`; // Mostramos la palabra en el contenedor
+  console.log(game.sel, game.scramble);
 });
+
+function sorter(word) {
+  let temp = word.split(""); // Separamos la palabra en letras
+  temp.sort(() => {
+    return 0.5 - Math.random();
+  }); // Ordenamos las letras de forma aleatoria
+  temp = temp.join(""); // Unimos las letras
+  console.log(temp); // Mostramos la palabra en consola
+  if (word === temp) {
+    console.log(word, temp);
+    return sorter(word); // Si la palabra es igual a la palabra ordenada, llamamos a la funcion sorter
+  }
+  return temp; // Retornamos la palabra ordenada
+}
