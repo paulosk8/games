61ea781d0f113bc630c2939443f740349f83f52b
Paulo Galarza <paulo-123@hotmail.es>
2025-03-15 15:53:41 -0500

    Agregado scoreBoard

--- scramble/scramble.js
+++ scramble/scramble.js
@@ -2,6 +2,11 @@ const gameArea = document.querySelector(".gameArea"); // Creamos el contenedor d
 const btn = document.createElement("button"); // Creamos el boton de inicio del juego
 const output = document.createElement("div"); // Creamos el contenedor de las palabras
 const inWord = document.createElement("input"); // Creamos el input para ingresar la palabra
+const scoreBoard = document.createElement("div"); // Creamos el contenedor del puntaje
+scoreBoard.textContent = "Puntaje: 0"; // Texto del puntaje
+scoreBoard.style.fontSize = "2em"; // Tamaño de la fuente
+scoreBoard.style.color = "#4C048C"; // Color del texto
+scoreBoard.style.backgroundColor = "#FFD700"; // Color de fondo
 inWord.setAttribute("type", "text"); // Tipo de input
 inWord.classList.add("myInput"); // Clase de bootstrap
 output.style.textAlign = "center"; // Alineamos el texto al centro
@@ -11,19 +16,28 @@ output.textContent = "Clic en el boton"; // Texto del contenedor de las palabras
 console.log(btn);
 
 // Agregar a la pagina del HTML
+gameArea.appendChild(scoreBoard);
 gameArea.appendChild(output);
 gameArea.appendChild(btn);
 gameArea.appendChild(inWord);
+
+// Elementos ocultos
+inWord.style.display = "none";
+scoreBoard.style.display = "none";
 
 // Valores iniciales del juego
 const myWords = ["cocodrilo", "gato", "raton"];
 const game = {
   sel: "",
   scramble: "",
+  correct: 0,
+  incorrect: 0,
 };
 // evento click del boton
 btn.addEventListener("click", function (e) {
   btn.style.display = "none"; // Ocultamos el boton
+  inWord.style.display = "inline"; // Mostramos el input
+  scoreBoard.style.display = "block"; // Mostramos el puntaje
   myWords.sort(() => {
     return 0.5 - Math.random();
   }); // Ordenamos las palabras de forma aleatoria
@@ -46,16 +60,29 @@ inWord.addEventListener("keypress", (e) => {
   }
 });
 
+function addScore() {
+  let tempOutput = `Correctas: <b>${game.correct}</b> VS incorrectas: <b>${game.incorrect}</b>`;
+  scoreBoard.innerHTML = tempOutput;
+}
+
 function winChecker(params) {
   inWord.style.borderWidth = "5px";
   if (inWord.value === game.sel) {
     console.log("Ganaste");
     inWord.style.borderColor = "green";
+    game.correct++;
+    addScore();
+    inWord.disabled = true;
+    btn.style.display = "inline";
+    btn.textContent = "Siguiente Palabra";
+    inWord.value = "";
   } else {
     inWord.style.borderColor = "red";
     console.log("Perdiste");
     inWord.value = "";
     inWord.focus();
+    game.incorrect++;
+    addScore();
   }
 }
 
